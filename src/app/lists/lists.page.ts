b1326cbf869c365ee389bc5df854158620f91ca1
Staant95 <antokhi.stas@gmail.com>
2020-04-27 12:20:51 +0200

    fixed token-interceptor

--- src/app/lists/lists.page.ts
+++ src/app/lists/lists.page.ts
@@ -12,4 +12,6 @@ export class ListsPage implements OnInit {
   ngOnInit() {
   }
 
+
+
 }
--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {from, Observable} from "rxjs";
 import {Storage} from "@ionic/storage";
-import {fromPromise} from "rxjs/internal-compatibility";
-import {mergeMap} from "rxjs/operators";
+import {switchMap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -14,17 +13,17 @@ export class TokenInterceptorService implements HttpInterceptor{
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    this.token = this.getToken();
-    console.log('after function call')
-    const header = req.headers.append('Authorization', 'Bearer ' + this.token);
-    let newRequest = req.clone({headers : header})
+    return from(this.storage.get('auth_token').then(
+        token => {
+          const header = req.headers.append('Authorization', 'Bearer ' + token);
+          return req.clone({headers: header});
+        })
+    ).pipe(
+        switchMap(newRequest => next.handle(newRequest))
+    );
 
-    return next.handle(newRequest);
   }
 
-  async getToken() {
-    console.log('in function')
-    return await this.storage.get('auth_token');
-  }
+
 
 }
