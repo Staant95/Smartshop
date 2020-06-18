867362932f4320696db2329835206481f0f8b3d3
MatteoDP <mpd96ita@gmail.com>
2020-06-18 12:25:50 +0200

    up creation list

--- src/app/lists/lists.page.ts
+++ src/app/lists/lists.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ShoppingListService} from "../services/shopping-list.service";
-import { PopoverController} from "@ionic/angular";
-import {CreateCardPopoverComponent} from "./components/create-card-popover/create-card-popover.component";
-import {BehaviorSubject} from "rxjs";
+import {ShoppingListService} from '../services/shopping-list.service';
+import { PopoverController} from '@ionic/angular';
+import {CreateCardPopoverComponent} from './components/create-card-popover/create-card-popover.component';
+import {BehaviorSubject} from 'rxjs';
 
 
 @Component({
@@ -24,8 +24,9 @@ export class ListsPage implements OnInit {
     this.shoplists.getAll().subscribe(
         data => {
           this.lists = data;
-          if(this.lists.length !== 0)
+          if (this.lists.length !== 0) {
             this.listLength$.next(true);
+          }
         }
     );
   }
@@ -35,8 +36,8 @@ export class ListsPage implements OnInit {
   onCardDelete(cardID, index) {
     this.shoplists.delete(cardID).subscribe(
         _ => {
-          this.lists.splice(index,1);
-          if(this.lists.length === 0) this.listLength$.next(false);
+          this.lists.splice(index, 1);
+          if (this.lists.length === 0) { this.listLength$.next(false); }
         }
     );
   }
@@ -46,23 +47,30 @@ export class ListsPage implements OnInit {
 
     const popover = await this.popoverCtrl.create({
       component: CreateCardPopoverComponent,
-      event: event
+      event
     });
 
     popover.onDidDismiss().then(data => {
-      if(data !== null){
-        if(data !== 'undefined'){
+      if (data !== null) {
+        if (data !== 'undefined') {
             // controlli se l'oggetto ha la chiave codice
             // chiami il service che aggiunge l'utente alla lista
             // con quel codice
 
+            var name = data["data"];
+            name =  (name["listName"]).replace(/"([^"]+)":/g, '$1:');
+            // tslint:disable-next-line:triple-equals
+            if ( name.length == 6 && name.charAt(0) == '#') {
+                // tslint:disable-next-line:prefer-const
 
-          this.shoplists.save(data.data['listName']).subscribe(
-              list => {
-                this.lists.push(list);
-                this.listLength$.next(true);
-              }
-          );
+            } else {
+                this.shoplists.save(data.data.listName).subscribe(
+                    list => {
+                        this.lists.push(list);
+                        this.listLength$.next(true);
+                    }
+                );
+            }
         }
       }
     });
